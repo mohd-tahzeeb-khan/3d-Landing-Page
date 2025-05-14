e0591a1d57605ff550335e33db713dc3f433afa8
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-15 02:59:04 +0530

    import the header in the App.jsx file

--- Robot Developer/src/App.jsx
+++ Robot Developer/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import Header from './components/header'
 const App = () => {
   return (
     <>
+    <Header/>
       <main>
       {/* Gradient Image */}
       <img className='absolute top-0 right-0 opacity-60 -z-10 ' src='/assets/gradient.png' alt='Gradient Image'/>
 
-      
+
       {/* Blur Effect */}
       <div className='h-0 w-[40rem] absolute top-20 right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10'></div>
       </main>
