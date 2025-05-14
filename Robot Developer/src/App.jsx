422a84120af6f4ce7611ef05ea082cd7ac35375f
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-05-15 04:28:44 +0530

    Final Website is Ready to Deploy

--- Robot Developer/src/App.jsx
+++ Robot Developer/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/header'
 import Hero from './components/hero'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration:1500,
+      once:true
+    })
+  })
+  
   return (
     <>
     <Header/>
-      <main>
+      <main className='sm:mb-20 md:mb-10'>
       {/* Gradient Image */}
       <img className='absolute top-0 right-0 opacity-60 -z-10 ' src='/assets/gradient.png' alt='Gradient Image'/>
 
